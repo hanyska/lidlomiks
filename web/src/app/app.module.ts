2fca2f54895c22e7aeae4fcd1be35c6a9377dd66
Anna Janusz <anna.janusz@loycon.pl>
2022-02-13 23:59:28 +0100

    Change Angular Material to Bootstrap

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule} from "@angular/material/toolbar";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatToolbarModule
     ],
   providers: [],
   bootstrap: [AppComponent]
